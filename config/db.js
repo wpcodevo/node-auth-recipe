f775e5874ef0f9a7e7ab9d77ef7c8fd2feb82b05
Ziddah Edem <ziddah.edem@gmail.com>
2020-10-18 08:07:58 +0000

    :tada: initial commit

--- config/db.js
+++ config/db.js
@@ -7,14 +7,13 @@ const DB = process.env.MONGODB_URI.replace(
 
 const localDB = process.env.MONGODB_LOCAL;
 const connectDB = async () => {
- 
-    const conn = await mongoose.connect(DB, {
-      useCreateIndex: true,
-      useFindAndModify: false,
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+  const conn = await mongoose.connect(localDB, {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 
-    // console.log(`Database connected successfully on ${conn.connection.host}`);
-  }
+  console.log(`Database connected successfully on ${conn.connection.host}`);
+};
 module.exports = connectDB;
--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const path = require('path');
 const dotenv = require('dotenv');
 
 process.on('uncaughtException', (err) => {
-  // console.log(`UNCAUGHT EXCEPTION Shutting down...`);
-  // console.log(err.name, err.message);
+  console.log(`UNCAUGHT EXCEPTION Shutting down...`);
+  console.log(err.name, err.message);
   process.exit(1);
 });
 
@@ -13,9 +13,9 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, () => {
-  // console.log(
-  //   `Server started successfully in ${process.env.NODE_ENV} mode on port: ${PORT}`
-  // );
+  console.log(
+    `Server started successfully in ${process.env.NODE_ENV} mode on port: ${PORT}`
+  );
 });
 
 process.on('unhandledRejection', () => {
